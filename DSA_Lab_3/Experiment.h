adda01efbb18ace8eb62db791d482a04fb58b757
Thanhhieu <147614636+pepemoe2@users.noreply.github.com>
2024-06-28 16:24:20 +0700

    Update Experiment.h
    add copydata, comand 1 and 4
    <chrono> might not be used

--- DSA_Lab_3/Experiment.h
+++ DSA_Lab_3/Experiment.h
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <chrono> //we chose chrono library over time.h because it provides a high resolution clock which help measuring run time more accurate
+//#include <chrono> we chose chrono library over time.h because it provides a high resolution clock which help measuring run time more accurate
 #include <unordered_map> // use for function convert name -> id
 #include "DataGenerator.h"
 #include "Algorithms.h"
@@ -23,3 +23,9 @@ int getAlgoId(const std::string& algo_cmd);
 string getInputOrder(int input_order_id);
 
 string getInputOrder(const std::string& input_order_cmd);
+
+int* CopyData(int* a, int size);
+
+void Command1(char* algo_name, char* input_filename, char* output_param);
+
+void Command4(char* algo_name1, char* algo_name2, char* input_filename);
